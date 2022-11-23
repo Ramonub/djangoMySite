36d083a8d41311f64ad42d56d0627db4ca3a8d58
Ramon Burgstad <1009381@hr.nl>
2022-11-23 15:14:59 +0100

    removed folder

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+     
+# class Sensor(models.Model):
+    
+#     name = models.CharField(max_length = 50)
+#     boardNumber = models.IntegerField()
+#     channel = models.IntegerField()
+#     ai_range = ULRange.BIP10VOLTS
+    
+#     def __str__(self):
+#         return self.name
+    
