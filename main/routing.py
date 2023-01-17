552e9a4578fddd34d63df7dd707c52d81cf2534b
Ramon Burgstad <1009381@hr.nl>
2023-01-17 15:20:06 +0100

    deleted files

--- main/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path, path
-
-from .consumers import WSConsumer
-
-ws_urlpatterns = [
-    re_path('ws/chart/(?P<room_name>\w+)/$', WSConsumer.as_asgi()),
-    re_path('ws/', WSConsumer.as_asgi()),
-]
