a453a507e3bef9efeef547927d7a9b080485901a
Ramon Burgstad <1009381@hr.nl>
2023-01-18 09:30:55 +0100

    Created setup steps

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 ## Django project setup guide
 
-Run setup.py and visit website on ip:port/show in the webbrowser
+1. Update and upgrade Raspberry pi
+-> sudo apt-get update && sudo apt-get upgrade
+
+2. Go to directory where you want to install
+
+3. Install git
+-> sudo apt-get install git
+
+4. Clone and enter repository
+-> git clone https://github.com/Ramonub/djangoMySite.git
+-> cd djangoMySite
+
+5. Install virtualenv
+-> pip install virtualenv
+
+6. Create and activate venv
+-> virtualenv venv -p python && source venv/bin/activate
+
+7. Run setup file
+-> python setup.py
+
+8. Open django interface in webbrowser
+-> http://192.168.252.32:8000/show
--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
-import shutil
+# import shutil
 import os
 
-os.system('sudo apt-get update && sudo apt-get upgrade')
-
-os.system('pip install virtualenv && virtualenv venv -p python')
-
-os.system('source venv/bin/activate')
-
+print('===== Install requirements')
 os.system('pip install -r requirements.txt')
 
+print('===== Moving services to the right place')
+os.system('sudo cp programFiles/django.service /usr/lib/systemd/system/')
+os.system('sudo cp programFiles/django_worker.service /usr/lib/systemd/system/')
 
-# Moving systemd to correct place
-shutil.copy('programFiles/django.service', '/usr/lib/systemd/system/')
-shutil.copy('programFiles/django_worker.service', '/usr/lib/systemd/system/')
-# Enable services
+print('===== Activate services')
 os.system('sudo systemctl enable /usr/lib/systemd/system/django.service')
 os.system('sudo systemctl enable /usr/lib/systemd/system/django_worker.service')
+
+if (os.path.exists('/usr/lib/systemd/system/django.service') and os.path.exists('/usr/lib/systemd/system/django_worker.service')):
+    print('Files succesfully moved')
+else:
+    print('Something went wrong, run commands by hand: ')
+    print('sudo cp programFiles/django.service /usr/lib/systemd/system/ && sudo cp programFiles/django_worker.service /usr/lib/systemd/system/')
+    print('sudo systemctl enable /usr/lib/systemd/system/django.service && sudo systemctl enable /usr/lib/systemd/system/django_worker.service')
+    
